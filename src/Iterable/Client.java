116e25c0e739dbe63c2e81de89fb92e18e851709
Prateek Vijay <113239008+pkvijay65@users.noreply.github.com>
2023-06-15 15:31:17 +0530

    implement iterable

--- src/Iterable/Client.java
+++ src/Iterable/Client.java
@@ -1,2 +1,25 @@
-package Iterable;public class Client {
+package Iterable;
+
+import java.util.Iterator;
+
+public class Client {
+  public static void main(String[] args) {
+    Node n5 = new Node(50, null);
+    Node n4 = new Node(40, n5);
+    Node n3 = new Node(30, n4);
+    Node n2 = new Node(20, n3);
+    Node n1 = new Node(10, n2);
+
+    // non-sugar way
+        Iterator<Integer> itr = n1.iterator();
+        while(itr.hasNext()){
+            int val = itr.next();
+            System.out.println(val);
+        }
+
+    // sugar way - also called as syntactic sugar
+//    for(int val: n1){
+//      System.out.println(val);
+//    }
+  }
 }
--- src/Iterable/Node.java
+++ src/Iterable/Node.java
@@ -1,2 +1,42 @@
-package Iterable;public class Node {
+package Iterable;
+
+import org.w3c.dom.traversal.NodeIterator;
+
+import java.util.Iterator;
+
+public class Node implements Iterable<Integer>{
+
+  private int data;
+  private Node next;
+
+  public Node(int data, Node next){
+    this.data = data;
+    this.next = next;
+  }
+
+  @Override
+  public Iterator<Integer> iterator() {
+    return new NodeIterator(this);
+  }
+
+  public static class NodeIterator implements Iterator<Integer>{
+
+    private Node current;
+
+    public NodeIterator(Node head){
+      this.current = head;
+    }
+
+    @Override
+    public boolean hasNext() {
+      return current != null;
+    }
+
+    @Override
+    public Integer next() {
+      int data = current.data;
+      current = current.next;
+      return data;
+    }
+  }
 }
