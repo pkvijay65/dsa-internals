b49698c69cf27b841ae075e54dbd44a9da9cc334
Prateek Vijay <113239008+pkvijay65@users.noreply.github.com>
2023-06-15 15:30:42 +0530

    implement queue by arraylist

--- src/Queue/Implentation_by_arraylist/Queue.java
+++ src/Queue/Implentation_by_arraylist/Queue.java
@@ -1,2 +1,32 @@
-package Queue.Implentation_by_arraylist;public class Queue {
+package Queue.Implentation_by_arraylist;
+
+import java.util.ArrayList;
+
+public class Queue {
+  ArrayList<Integer> list = new ArrayList<Integer>();
+
+  public void enque(int data) {
+    list.add(data);
+  }
+
+  public int deque() {
+    if (list.isEmpty()) {
+      throw new RuntimeException("Queue is empty");
+    } else {
+      int temp = list.get(0);
+      list.remove(0);
+      return temp;
+    }
+  }
+
+  public static void main(String[] args) {
+    Queue queue = new Queue();
+    queue.enque(1);
+    queue.enque(2);
+    queue.enque(3);
+    queue.enque(4);
+    queue.enque(5);
+    System.out.println("first data point in queue is :: " +  queue.deque());
+  }
+
 }
